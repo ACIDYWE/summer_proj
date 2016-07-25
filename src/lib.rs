7de509b0d66e65b127976980eb82fc84db6bb4f8
Obryv Mostov <mostobriv@MacBook-Pro-Obryv.local>
2016-07-25 19:27:58 +0700

    added result struct to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -5,19 +5,30 @@ use std::net::TcpStream;
 use std::io::Read;
 
 pub trait ReadlineForTcpStream {
-    fn read_line(&mut self, buf: &mut String) -> Option<usize>;
+    fn read_line(&mut self, buf: &mut String) -> Result<usize>;
 }
 
-impl ReadlineForTcpStream for TcpStream {
-    fn read_line(&mut self, buf: &mut String) -> Option<usize> {      //reading until new line
-        let mut temp: [u8;1] = [0;1];
-        let mut readen = 0;
-        self.read_exact(&mut temp).unwrap();
-        while temp[0] != 10 {
-            readen+=1;
-            (*buf).push(temp[0] as char);
-            self.read_exact(&mut temp).unwrap();
-        }
-        Some(readen)
-    }
+pub enum Readline_error {
+    NOT_ASCII,
+    CONNECTION_LOST,
 }
+
+
+impl ReadlineForTcpStream for TcpStream {
+    fn read_line(&mut self, buf: &mut String) -> Result<usize> {      //reading until new line
+        let mut c: [u8;1] = [0;1];
+        let mut readen = 0;
+        self.read_exact(&mut c).unwrap();
+        while c[0] != 10 {
+            if !c[0].is_ascii() {
+                return Err(Readline_error::NOT_ASCII);
+            }
+            readen+=1;
+            (*buf).push(c[0] as char);
+            match self.read_exact(&mut c) {
+                Err(_) => return Err(Readline_error::CONNECTION_LOST)
+            }
+        }
+        Ok(readen)
+    }
+}i
