d5d7896572e3a33f363f1d452cf27635563721af
Maks <mostobriv@gmail.com>
2016-07-27 03:14:30 +0700

    finished registration

--- src/client/order_reg.rs
+++ src/client/order_reg.rs
@@ -4,6 +4,7 @@ use super::Client;
 
 use std::io::Write;
 use std::net::TcpStream;
+use ::ReadlineForTcpStream;
 
 pub trait OrderRegPage {
 	fn order_reg(&mut self) -> ();
@@ -14,9 +15,33 @@ struct Order {
 	text: String
 }
 
+
 impl<'a> OrderRegPage for Client<'a> {
 	fn order_reg(&mut self) -> () {
-		self.stream.write(b"SOSI PISUN\n");
+		self.stream.write(b"Enter yout order here:\n\
+							> ").unwrap();
+		let mut buffer = String::new();
+		let len = self.stream.read_line(&mut buffer).unwrap();
+		if len == 0 {
+			self.stream.write(b"NANANANANANANA U MUST CHOOSE SOMETHING PIDR!!!!11\n").unwrap();
+			return; //return user to the main page
+		}
+		for i in buffer.as_bytes() {
+			if !(*i >= 48 && *i <= 57 || *i >= 97 && *i <= 102) {
+				self.stream.write(b"DO U LIKE THE KKK!?!?!?!?\n\
+									YOU WROTE SOME SHITTY SYMBOLS!!!\n");
+				return; //return user to the main page
+			}
+		}
+		let c_order = Order { client_id: self.client_id as i32,
+								   text: buffer };
+
+		let pool = self.conn.lock().unwrap();
+		pool.prep_exec("INSERT INTO shawerma.orders
+							(client_id, text)
+						VALUES
+							(:client_id, :text)", (c_order.client_id, c_order.text)).unwrap();
+
+
 	}
 }
-
